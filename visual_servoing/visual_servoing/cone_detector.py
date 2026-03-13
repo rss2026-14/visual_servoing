b10f7803fd2f70bddbdd3a377da8c771e6616f65
crystalmelon <cheniseoharper@gmail.com>
2026-03-13 16:48:12 +0000

    moved function for ease

--- visual_servoing/visual_servoing/cone_detector.py
+++ visual_servoing/visual_servoing/cone_detector.py
@@ -5,6 +5,7 @@ from rclpy.node import Node
 import numpy as np
 
 import cv2
+
 from cv_bridge import CvBridge, CvBridgeError
 
 from sensor_msgs.msg import Image
@@ -12,8 +13,39 @@ from geometry_msgs.msg import Point #geometry_msgs not in CMake file
 from vs_msgs.msg import ConeLocationPixel
 
 # import your color segmentation algorithm; call this function in ros_image_callback!
-import color_segmentation
-from color_segmentation import cd_color_segmentation
+# from color_segmentation import cd_color_segmentation
+def cd_color_segmentation(img, template):
+    """
+    Implement the cone detection using color segmentation algorithm
+    Input:
+        img: np.3darray; the input image with a cone to be detected. BGR.
+        template: Not required, but can optionally be used to automate setting hue filter values.
+    Return:
+        bbox: ((x1, y1), (x2, y2)); the bounding box of the cone, unit in px
+            (x1, y1) is the top left of the bbox and (x2, y2) is the bottom right of the bbox
+    """
+    ########## YOUR CODE STARTS HERE ##########
+    HSV_img=cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    lower_bound = np.array([5, 150, 90])
+    upper_bound = np.array([50, 255, 255])
+    cone_mask=cv2.inRange(HSV_img,lower_bound,upper_bound) #Masks image - any orange pixel is 1(white), and everything else is 0(black)
+
+    kernel = np.ones((5, 5), np.uint8) #5x5 of 1s
+    eroded = cv2.erode(cone_mask, kernel, iterations=2) #For each pixel, if any neighbor in a 5x5 isn't included in the mask, removes pixel from mask
+    dilated = cv2.dilate(eroded, kernel, iterations=2) #For each pixel, if any neighbor in a 5x5 is still included in the mask, adds pixel to mask
+
+    contours, hierarchy = cv2.findContours(dilated, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE) #makes contour of object boundary
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)
+    x1, y1, w, h = cv2.boundingRect(contours[0]) #makes rectangle around contour
+
+    bounding_box = ((x1, y1), (x1+w, y1+h))
+
+    ########### YOUR CODE ENDS HERE ###########
+
+    # Return bounding box
+    return bounding_box
+
 
 
 class ConeDetector(Node):
