ae13030430518e744618b20d69a636fa1f3f7f5a
Hoives <pwmason@mit.edu>
2026-03-14 18:10:03 +0000

    edited color segmentation of cone_detector

--- visual_servoing/visual_servoing/cone_detector.py
+++ visual_servoing/visual_servoing/cone_detector.py
@@ -27,15 +27,15 @@ def cd_color_segmentation(img, template):
     ########## YOUR CODE STARTS HERE ##########
     HSV_img=cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
 
-    lower_bound = np.array([5, 150, 120])
+    lower_bound = np.array([0, 80, 110])
     upper_bound = np.array([50, 255, 255])
     # lower_bound = np.array([10, 130, 100])
     # upper_bound = np.array([25, 255, 255])
     cone_mask=cv2.inRange(HSV_img,lower_bound,upper_bound) #Masks image - any orange pixel is 1(white), and everything else is 0(black)
 
     kernel = np.ones((3, 3), np.uint8) #5x5 of 1s
-    eroded = cv2.erode(cone_mask, kernel, iterations=1) #For each pixel, if any neighbor in a 5x5 isn't included in the mask, removes pixel from mask
-    dilated = cv2.dilate(eroded, kernel, iterations=1) #For each pixel, if any neighbor in a 5x5 is still included in the mask, adds pixel to mask
+    eroded = cv2.erode(cone_mask, kernel, iterations=2) #For each pixel, if any neighbor in a 5x5 isn't included in the mask, removes pixel from mask
+    dilated = cv2.dilate(eroded, kernel, iterations=2) #For each pixel, if any neighbor in a 5x5 is still included in the mask, adds pixel to mask
 
     # contours, hierarchy = cv2.findContours(dilated, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE) #makes contour of object boundary
     # contours = sorted(contours, key=cv2.contourArea, reverse=True)
