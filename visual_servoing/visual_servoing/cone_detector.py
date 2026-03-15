71517295741089fb2a74ccfaa0dfb55261b6bb4d
Hoives <pwmason@mit.edu>
2026-03-15 21:32:18 +0000

    added separate color segment for lines

--- visual_servoing/visual_servoing/cone_detector.py
+++ visual_servoing/visual_servoing/cone_detector.py
@@ -18,7 +18,7 @@ def cd_color_segmentation(img, template, line):
     """
     Implement the cone detection using color segmentation algorithm
     Input:
-        img: np.3darray; the input image with a cone to be detected. BGR.
+        img: np.3darray; the input image with a cone to be detected. BGR
         template: Not required, but can optionally be used to automate setting hue filter values.
     Return:
         bbox: ((x1, y1), (x2, y2)); the bounding box of the cone, unit in px
